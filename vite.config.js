8bb9f0cf3192cae6f78ee7eb79adeec6e2ccb2cc
Danilvin <108340803+Danilvin@users.noreply.github.com>
2026-05-18 02:02:20 -0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],    
+  plugins: [react(), tailwindcss()],
+  bsae: "/OlderburskihFamily-frontend-2026s"    
 })
